43dd0fe15846608683aab70aec3012ddcd3a0b87
harsh-agrawal72 <harsagrawal7270@gmail.com>
2025-09-01 10:47:33 +0530

    Update class_pattern.py

--- patterns/class_pattern.py
+++ patterns/class_pattern.py
@@ -1,1 +1,126 @@
+# Question 1
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*",end=" ")
+#     print(" ")
+
+# Question 2
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+# Question 3
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+# Question 4
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         if(j%2==0):
+#             print("*",end="")
+#         else:
+#             print(j,end="")
+#     print()
+
+# Question 5
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(j==1):
+#             print("*",end="")
+#         elif(j==5):
+#             print("*",end="")
+#         else:
+#             print("_",end="")
+#     print()
+
+
+# Question 6
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         if j == 0 or j == n - i - 1:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+
+# Question 9
+# n = int(input("enter any number: "))
+# for i in range(1,n+1):
+#     for j in range(n-i+1):
+#         print("*",end="")
+#     for j in range(2*i-2):
+#         print(" ",end="")
+#     for j in range(n-i+1):
+#         print("*",end="")
+#     print()
+
+# Question 10
+# n = int(input("enter any number: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end="")
+#     for j in range(2*n-2*i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+# Question 11
+# n = int(input("enter any number: "))
+# for i in range(1,n+1):
+#     for j in range(n-i+1):
+#         print("*",end="")
+#     for j in range(2*i-2):
+#         print(" ",end="")
+#     for j in range(n-i+1):
+#         print("*",end="")
+#     print()
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end="")
+#     for j in range(2*n-2*i):
+#         print(" ",end="")
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+
+# Question 12
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()
+
+
+# Question 13
+# for i in range(5,0,-1):
+#     for j in range(1,i):
+#         print(j, end=" ")
+#     print()
+
+
+# Question 14
+# num = 1
+# for i in range(1,5):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num+=1
+#     print()
+
+
+# Question 15
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+# for i in range(4,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
 
